edc600cf8e8a3a76dd9d9e960525562c1caadd30
Tom Danvers <32431576+TomD53@users.noreply.github.com>
2023-02-06 22:10:02 +0000

    remove dependencies (experimental)

--- examples/direct_read.cpp
+++ examples/direct_read.cpp
@@ -10,8 +10,8 @@
 
 // The LSM6DSO32 can be used with either SPI or I2C, and this library supports both, using Protocol: https://github.com/TeamSunride/Protocol
 #define CS_pin 10
-LSM6DSO32 LSM(CS_pin, SPI, 4000000); // spi protocol constructor
-// LSM6DSO32 LSM(&Wire, 1000000); // i2c protocol constructor
+//LSM6DSO32 LSM(CS_pin, SPI, 4000000); // spi protocol constructor
+LSM6DSO32 LSM(&Wire, 1000000); // i2c protocol constructor
 
 
 void setup() {
@@ -25,15 +25,16 @@ void setup() {
     LSM.default_configuration();
     // for example:
     // LSM.set_interrupts_active_low(false);
+    LSM.set_accel_ODR(OUTPUT_DATA_RATES::ODR_833_HZ);
 }
 
 void loop() {
     // directly read from the accel and gyro registers - not using the fifo
     Vector<double, 3> acc = LSM.get_accel();
     Vector<double, 3> gyr = LSM.get_gyro();
-    Serial.printf("Acc: %lf, %lf, %lf\n     Gyr: %lf, %lf, %lf\n", acc[0], acc[1], acc[2], gyr[0], gyr[1], gyr[2]);
+    Serial.printf("%lf, %lf, %lf\n", acc[0], acc[1], acc[2]);
 
     // Be aware of the ODR (Output data rate) that is set - reading at a rate higher than this means you will be reading stale data when using direct reads.
     // Note: The ODR can be set in setup() using e.g.    LSM.set_accel_ODR(OUTPUT_DATA_RATES::ODR_833_HZ);    and     LSM.set_gyro_ODR(OUTPUT_DATA_RATES::ODR_833_HZ);
-    delay(10);
+    delay(1);
 }
